85172d2d0101407afc4464342fe4f5ffda76fcff
oreotarot <ohoh241109@gmail.com>
2026-03-14 22:03:36 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'tarot-v1';
+const FILES = [
+  './tarot.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
